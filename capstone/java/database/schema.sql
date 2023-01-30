5adb44c3e21c65f1ed02d715cd82b1acc219a0ca
Justin Myers <jm317708@gmail.com>
2023-01-30 14:27:54 -0500

    Added Profile, meal, food, and meal_food tables

--- capstone/java/database/schema.sql
+++ capstone/java/database/schema.sql
@@ -23,4 +23,27 @@ CREATE TABLE profile (
    CONSTRAINT FK_user FOREIGN KEY (user_id) REFERENCES users(user_id)
  );
 
+ CREATE TABLE meal(
+ meal_id SERIAL,
+ user_id INTEGER NOT NULL,
+ meal_type varchar(100),
+ CONSTRAINT PK_meal PRIMARY KEY (meal_id),
+ CONSTRAINT FK_user FOREIGN KEY (user_id) REFERENCES users(user_id)
+ );
+
+ CREATE TABLE food(
+ food_id SERIAL,
+ food_name VARCHAR(100) NOT NULL,
+ calories NUMERIC(10,2) NOT NULL,
+ CONSTRAINT PK_food PRIMARY KEY (food_id)
+ );
+
+ CREATE TABLE meal_food(
+ meal_id INTEGER NOT NULL,
+ food_id INTEGER NOT NULL,
+ CONSTRAINT PK_meal_food PRIMARY KEY (meal_id, food_id),
+ CONSTRAINT FK_meal_id FOREIGN KEY (meal_id) REFERENCES meal(meal_id),
+ CONSTRAINT FK_food_id FOREIGN KEY (food_id) REFERENCES food(food_id)
+ );
+
 COMMIT TRANSACTION;
