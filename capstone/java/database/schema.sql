9888a46e10e234372eec4676f3f12c8e9bcf0917
Justin Myers <jm317708@gmail.com>
2023-01-30 13:46:15 -0500

    Added profile table to database

--- capstone/java/database/schema.sql
+++ capstone/java/database/schema.sql
@@ -10,4 +10,17 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
+CREATE TABLE profile (
+   profile_id SERIAL,
+   user_id INTEGER NOT NULL,
+   email varchar(100) NOT NULL,
+   age INTEGER NOT NULL,
+   height_feet INTEGER NOT NULL,
+   height_inches INTEGER NOT NULL,
+   current_weight NUMERIC(5,2) NOT NULL,
+   desired_weight NUMERIC(5,2) NOT NULL,
+   CONSTRAINT PK_profile PRIMARY KEY (profile_id),
+   CONSTRAINT FK_user FOREIGN KEY (user_id) REFERENCES users(user_id)
+ );
+
 COMMIT TRANSACTION;
