147ff0b6be912ffe39080c3337bf9cd53040b69a
Jacob Pavkov <jcp1359@gmai.com[D[D[D[Dl.com>
2023-01-30 17:08:40 -0500

    TrackFood.vue created in views. FoodTrackerForm created in components

---
+++ capstone/vue/src/components/FoodTrackerForm.vue
@@ -0,0 +1,28 @@
+<template>
+    <form action="">
+        <!-- <header> Meeting Goals Together </header> -->
+        <div>
+             Food Type <input type="text">
+        </div>
+       <div>
+           Calories <input type="number">
+       </div>
+        <div>
+            Carbs <input type="number">
+        </div>
+        <div>
+            Protien <input type="number">
+        </div>
+        <div>
+            Fats <input type="number">
+        </div>
+        <div>
+            Fiber <input type="number">
+        </div>
+        <div>
+            <button> Submit </button>
+            <button> Add to Quick add</button>
+        </div>
+    </form>
+    
+</template>
