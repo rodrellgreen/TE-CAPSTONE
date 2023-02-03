9c3f72a0a046423b70676a413573a3eea057bd34
Rodrell Green <rodrellgreen@gmail.com>
2023-02-03 16:55:04 -0500

    end of day

--- capstone/vue/src/components/FoodDiary.vue
+++ capstone/vue/src/components/FoodDiary.vue
@@ -28,11 +28,6 @@
     <td>{{food.fats}}</td>
     <td>{{food.fiber}}</td>
     <td>{{food.servingSize}}</td>
-<<<<<<< HEAD
-=======
-    <td>{{food.date}}</td>
-    
->>>>>>> main
   </tr>
 </tbody>
  
