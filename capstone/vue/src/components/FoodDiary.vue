d027cc19f9bc1b73ea19c5e010ea17414594eb2d
Corey Stamper <cstamper93@gmail.com>
2023-02-03 15:31:56 -0500

    Created mealdao and jdbcmealdao

--- capstone/vue/src/components/FoodDiary.vue
+++ capstone/vue/src/components/FoodDiary.vue
@@ -25,14 +25,8 @@
     <td>{{food.fats}}</td>
     <td>{{food.fiber}}</td>
     <td>{{food.servingSize}}</td>
-<<<<<<< HEAD
     <td>{{food.date}}</td>
-
-
     
-    
-=======
->>>>>>> main
   </tr>
 </tbody>
  
