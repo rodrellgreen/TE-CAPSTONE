d82659aa60a8031dd6c6410b088e860628f2d147
Jacob Pavkov <jcp1359@gmai.com[D[D[D[Dl.com>
2023-02-08 23:32:13 -0500

    updated twilio in backend. everything is working well now and backend should be completly done. needs alittle more style on front end though

--- capstone/java/src/main/java/com/techelevator/Twilio/TwilioService.java
+++ capstone/java/src/main/java/com/techelevator/Twilio/TwilioService.java
@@ -20,15 +20,17 @@ public class TwilioService {
     private String auth_token2 = System.getenv("cc7cc507dc5cc63670185ff2f3060bbd");
 //
     private String fromPhoneNumber = "+18887911936";
-    private String positiveMessage = "Don't forget to log your food. Your doing Great :)";
+    private String positiveMessage = "Hello From Tech Fitness Pal! THE BETTER ONE! *Trademark still pending* Don't forget to log your food. You're doing GREAT :) Keep up the good work!";
+    private String lessThan15Message = "Hello From Tech Fitness Pal! THE BETTER ONE! *Trademark still pending* Looks like you tried to set a reminder for yourself. Unfortunately you can't set messages less than 15 minutes from the future. Please reenter your desired date and time. ";
 
     public void sendMessage(String toPhoneNumber) {
 
-        Twilio.init( auth_token, account_sid);
+        Twilio.init( account_sid,  auth_token);
 
-        Message sms = Message.creator(new PhoneNumber(toPhoneNumber),
-                new PhoneNumber(fromPhoneNumber),
-                positiveMessage).create();
+        Message message = Message.creator(
+                new com.twilio.type.PhoneNumber(toPhoneNumber),
+                new com.twilio.type.PhoneNumber(fromPhoneNumber),
+                lessThan15Message).create();
     }
 
     public void sendMessageAtTime(String toPhoneNumber, LocalDateTime dt) {
